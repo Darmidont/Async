ecb985e2d566a2e8e60b8cd5355a6edac7d5b3f4
Dra <dara_r@rambler.ru>
2014-04-21 12:53:28 +0300

    temp test

--- MyTestThreads/Program.cs
+++ MyTestThreads/Program.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MyTestThreads
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var task = new Task(TestProcessAsync);
+            task.Start();
+
+            var task1 = new Task(TestQuickProcessAsync);
+            task1.Start();
+
+            task.Wait();
+            task1.Wait();
             Console.WriteLine("End a program");
             Console.ReadLine();
+        }
+
+        private static async void TestProcessAsync()
+        {
+            Console.WriteLine("Start async process");
+            Thread.Sleep(2000);
+            Console.WriteLine("End  async process");
+        }
+
+        private static async void TestQuickProcessAsync()
+        {
+            Console.WriteLine("Start quick async process");
+            Thread.Sleep(1000);
+            Console.WriteLine("End quick async process");
         }
     }
 }
